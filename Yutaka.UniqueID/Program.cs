cd3b03c8bf00c06f39be5544a2fa62dc3e073c77
blizmanyu <blizman2017@gmail.com>
2018-06-11 14:44:44 -0700

    2018 0611 1444

--- Yutaka.UniqueID/Program.cs
+++ Yutaka.UniqueID/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Yutaka.UniqueID
@@ -16,7 +14,26 @@ namespace Yutaka.UniqueID
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			//Application.Run(new Form1());
+			var now = DateTime.Now;
+			Clipboard.SetText(now.ToString("yyyy MMdd HHmm ssff"));
+			ShowBalloonTip(body: String.Format("Current time copied to clipboard!\n{0}", now.ToString("M/d/yyyy HH:mmtt").ToLower()));
+			Application.Exit();
+		}
+
+		private static void ShowBalloonTip(string title = null, string body = null, int timeout = 2200)
+		{
+			using (var notifyIcon = new NotifyIcon()) {
+				if (title != null)
+					notifyIcon.BalloonTipTitle = title;
+
+				if (body != null)
+					notifyIcon.BalloonTipText = body;
+
+				notifyIcon.Visible = true;
+				notifyIcon.Icon = SystemIcons.Application;
+				notifyIcon.ShowBalloonTip(timeout);
+			}
 		}
 	}
 }
